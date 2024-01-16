fcf58ab57d0b9512aca30d8ad8e5104e1e5bb475
Andrew Cho <cisdell@gmail.com>
2024-01-16 12:09:14 -0500

    added bare bone templates

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+import { mainModule } from "process";
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint neque
+        magni saepe. Iusto eum asperiores totam repellat hic odit consequatur
+        fugit sed fugiat. Eos tenetur cumque delectus exercitationem atque iure,
+        distinctio ullam, quo reiciendis laudantium alias! Tempora vero
+        cupiditate at quis nam ab dolor excepturi cumque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quas,
+        tempore labore voluptatum ipsam minima asperiores molestiae harum
+        reprehenderit quasi magni optio ea eius dolor, porro totam id tempora
+        distinctio quam accusantium possimus. Mollitia rem, maiores nemo quo
+        cupiditate exercitationem libero provident praesentium vero ad? Maiores,
+        doloribus. Commodi quis saepe doloribus molestias officia beatae
+        repellat quos! Esse nisi nemo inventore, officia, ad vel quam est eos
+        pariatur, libero nostrum officiis non. Impedit adipisci amet sunt itaque
+        est, fuga minima aliquam doloribus! Nam praesentium omnis et sequi!
+        Minus dolor voluptatum veritatis at! Ratione rerum repellendus
+        temporibus. Libero voluptatum fugit accusantium enim.
+      </p>
+    </main>
+  );
+}
