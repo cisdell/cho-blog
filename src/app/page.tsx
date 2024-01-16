93925ae7905da4d8df1320fd814fc300f1eda07b
Andrew Cho <cisdell@gmail.com>
2024-01-16 12:29:14 -0500

    async function added to fetch the data from mockend service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import PostCard from "@/components/PostCard";
 
-export default function Home() {
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+
+  if (!res.ok) {
+    console.log("could not fetch the posts");
+  }
+  return res.json();
+};
+
+export default async function Home() {
+  const posts = await fetchPosts();
   return (
     <main>
       <h2>Home</h2>
